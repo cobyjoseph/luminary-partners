f0db229273a316fcb46b6ed6bd0c0f2895546567
Coby Joseph <coby.joseph@gmail.com>
2023-03-10 17:20:09 -0300

    updates to reports section

--- src/lib/components/Reports.svelte
+++ src/lib/components/Reports.svelte
@@ -1,14 +1,16 @@
-<div class="heightClamp bg-white overflow-x-auto">
-	<div class="flex  gap-4 p-2 text-xs sm:text-sm  font-semibold">
-		<div class="h-full w-2 bg-secondaryLight" />
-		<div class="flex flex-col widthClamp ">
-			<div class="flex">Implementing Agile Methodologies for Enhanced Project Management</div>
+<div class=" bg-white overflow-x-auto">
+	<div class="flex   text-xs sm:text-sm py-2 font-semibold 2xl:justify-center 2xl:text-lg">
+		<div class="py-1 px-3 text-secondaryLight  font-bold">LATEST REPORTS</div>
+		<div
+			class="flex flex-col min-w-[10rem] justify-between  sm:min-w-[13rem] 2xl:min-w-[20rem] border-l-[1.5px] py-1 px-3  border-l-secondaryLight "
+		>
+			<div class="flex">Implementing Agile Methodologies</div>
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
 				fill="none"
 				viewBox="0 0 28 20"
 				stroke-width="1.5"
-				class="w-5 h-5 flex self-end stroke-secondaryDark "
+				class="w-5 h-5 flex self-end stroke-secondaryLight "
 			>
 				<path
 					stroke-linecap="round"
@@ -18,14 +20,16 @@
 			</svg>
 		</div>
 
-		<div class="flex flex-col widthClamp">
-			<div class="flex">Implementing Agile Methodologies for Enhanced Project Management</div>
+		<div
+			class="flex flex-col min-w-[10rem] justify-between  sm:min-w-[13rem] 2xl:min-w-[20rem] border-l-[1.5px]  border-l-secondaryLight py-1 px-3"
+		>
+			<div class="flex">Maximizing the Impact of CSR Programs</div>
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
 				fill="none"
 				viewBox="0 0 28 20"
 				stroke-width="1.5"
-				class="w-5 h-5 flex self-end stroke-secondaryDark "
+				class="w-5 h-5 flex self-end stroke-secondaryLight "
 			>
 				<path
 					stroke-linecap="round"
@@ -35,14 +39,54 @@
 			</svg>
 		</div>
 
-		<div class="flex flex-col widthClamp">
-			<div class="flex">Implementing Agile Methodologies for Enhanced Project Management</div>
+		<div
+			class="flex flex-col min-w-[10rem] justify-between  sm:min-w-[13rem] 2xl:min-w-[20rem] border-l-[1.5px]  border-l-secondaryLight py-1 px-3"
+		>
+			<div class="flex">Industry Trends in Supply Chain Management</div>
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
 				fill="none"
 				viewBox="0 0 28 20"
 				stroke-width="1.5"
-				class="w-5 h-5 flex self-end stroke-secondaryDark "
+				class="w-5 h-5 flex self-end stroke-secondaryLight "
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5"
+				/>
+			</svg>
+		</div>
+
+		<div
+			class="flex flex-col min-w-[10rem] justify-between  sm:min-w-[13rem] 2xl:min-w-[20rem] border-l-[1.5px]  border-l-secondaryLight py-1 px-3"
+		>
+			<div class="flex">Digital Transformation Strategies for SMEs</div>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 28 20"
+				stroke-width="1.5"
+				class="w-5 h-5 flex self-end stroke-secondaryLight "
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5"
+				/>
+			</svg>
+		</div>
+
+		<div
+			class="flex flex-col min-w-[10rem] justify-between  sm:min-w-[13rem] 2xl:min-w-[20rem] border-l-[1.5px]  border-l-secondaryLight py-1 px-3"
+		>
+			<div class="flex">Optimizing Performance through Cost Reduction</div>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 28 20"
+				stroke-width="1.5"
+				class="w-5 h-5 flex self-end stroke-secondaryLight "
 			>
 				<path
 					stroke-linecap="round"
@@ -56,7 +100,7 @@
 
 <style>
 	.widthClamp {
-		width: clamp(10rem, 15rem, 18rem);
+		width: clamp(10rem, 8vw + 20rem, 25rem);
 	}
 
 	.heightClamp {
