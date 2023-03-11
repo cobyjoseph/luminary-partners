51ab82fd6cc00966c7def7175257028effff2f43
Coby Joseph <coby.joseph@gmail.com>
2023-03-11 20:56:35 -0300

    grid

--- src/lib/components/Services.svelte
+++ src/lib/components/Services.svelte
@@ -1,25 +1,25 @@
-<div class="relative heightClass">
+<div class="relative heightClass ">
 	<div class="bg-base absolute h-full w-full" />
 
-	<div class="grid grid-col-2 grid-rows-2 h-full w-full z-10 relative">
-		<div class="  outline">image here</div>
-		<div class="   flex flex-col justify-center outline">
+	<div class=" grid-cols-2 m-auto grid h-full gap-4  z-10 relative outline outline-blue-400">
+		<div class=" ">image here</div>
+		<div class="   justify-center flex flex-col bg-green-400 outline px-8 ">
 			<div class="text-3xl font-satoshi font-extrabold text-primaryDark">
-				Developing a Roadmap for Success
+				Developing a roadmap for success
 			</div>
 			<div class="text-normal font-satoshi font-normal">
 				Align your business goals, resources, and initiatives to drive growth and profitability.
 			</div>
 		</div>
-		<div class="   flex flex-col justify-center outline">
+		<div class=" outline justify-center flex flex-col px-8">
 			<div class="text-3xl font-satoshi font-extrabold text-primaryDark">
-				Streamlining Your Business Processes
+				Streamlining Your business processes
 			</div>
 			<div class="text-normal font-satoshi font-normal">
 				Identify inefficiencies, reduce costs, and improve performance with data-driven insights.
 			</div>
 		</div>
-		<div class="  outline">image here</div>
+		<div class="  ">image here</div>
 	</div>
 </div>
 
