bf3ad5fc421fe410a51c04936d68fa937a971ccb
Coby Joseph <coby.joseph@gmail.com>
2023-03-16 19:23:05 -0300

    closer to parallax

--- src/lib/components/Services.svelte
+++ src/lib/components/Services.svelte
@@ -3,15 +3,18 @@
 	import bgLinesPortrait from '$lib/assets/patterns/portrait-background-lines-v0.2.svg';
 	import InView from '$lib/components/InView.svelte';
 
-	let color = 'blue';
+	let colorBlue = 'blue';
+	let colorRed = 'red';
 	let tempScroll = 400;
 	let scroll;
-	let speed = 0.8;
+	let speed = 0.4;
 </script>
 
 <svelte:window bind:scrollY={scroll} />
 
 {scroll}
+
+<!-- The issue is just that I need to reset scroll to 0 when at the top of the second page. so I need to make it scroll - the outerHeight of the first two pages. So just need to bind outerheight of those and use a store? or some other method to pass that value between the components -->
 
 <div class="heightClass relative overflow-hidden">
 	<img class="absolute z-10 bg-cover portrait:hidden" src={bgLines} alt="backgroundlines" />
@@ -24,13 +27,9 @@
 	<InView let:isVisible yThreshold="-400">
 		<div class="flex justify-center">
 			<div
-				style:color
-				style:transform={isVisible
-					? `'{translateY(${scroll - 500 * speed}px)}'`
-					: `'(translateY(0px))'`}
-				class=" relative z-20 px-[8%] font-satoshi text-3xl font-extrabold text-secondaryDark {isVisible
-					? 'customTranslate bg-red-400'
-					: ' bg-green-500'}"
+				style:color={isVisible ? `${colorRed}` : 'blue'}
+				style:transform={isVisible ? `translateY(${scroll * speed}px)` : `translateY(0px)`}
+				class=" relative z-20 px-[8%] font-satoshi text-3xl font-extrabold text-secondaryDark"
 			>
 				{isVisible}
 				Your partner for:
@@ -40,10 +39,6 @@
 </div>
 
 <style>
-	.customTranslate {
-		transform: translateY('`${scroll}px`');
-	}
-
 	.heightClass {
 		min-height: 100vh;
 		min-height: 100svh;
